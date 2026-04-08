9c62143be89711f8a4661643c302bbc60d142fb9
Jhostin Josue Quijije Aucancela <jhostinjosue23@gmail.com>
2026-04-08 17:32:59 +0000

    :sparkles: implementar pantallas de historial y perfil

---
+++ src/screens/RecentLogsScreen.tsx
@@ -0,0 +1,27 @@
+import React, { useCallback, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import ListTemplate from '../components/templates/ListTemplate';
+import RecentLogsList from '../components/organisms/RecentLogsList';
+import { attendanceService } from '../services/attendanceService';
+import type { AttendanceLog } from '../types/models';
+
+export default function RecentLogsScreen() {
+  const [logs, setLogs] = useState<AttendanceLog[]>([]);
+
+  const load = useCallback(async () => {
+    const data = await attendanceService.recent();
+    setLogs(data);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      load();
+    }, [load])
+  );
+
+  return (
+    <ListTemplate title="Historial" subtitle="Eventos recientes del control de ingreso">
+      <RecentLogsList logs={logs} />
+    </ListTemplate>
+  );
+}
